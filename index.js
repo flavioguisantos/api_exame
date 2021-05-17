d9f32c1e56128758969e9bd1e7e59b3c8ba5d657
FLAVIO GUILHERME DOS SANTOS <81536682+flavioguisantos@users.noreply.github.com>
2021-05-17 09:08:13 -0300

    Update index.js

--- index.js
+++ index.js
@@ -9,5 +9,5 @@ app.use(express.json())
 app.use(cors())
 app.use(routes)
 
-var port_number = server.listen(process.env.PORT || 3000)
+var port_number = app.listen(process.env.PORT || 3000)
 app.listen(port_number)
